923074cb6f6362bb3dd92d503b60f89e21081194
Gabriel Duarte <Gabriel.Duarte@tekstream.com>
2022-01-20 08:52:59 -0500

    Modify timeout connection

--- src/main/java/com/backendtest/inditex/zararetail/config/RestTemplateConfig.java
+++ src/main/java/com/backendtest/inditex/zararetail/config/RestTemplateConfig.java
@@ -11,8 +11,8 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(20000);
-        factory.setReadTimeout(20000);
+        factory.setConnectTimeout(60000);
+        factory.setReadTimeout(60000);
         return new RestTemplate(factory);
     }
 }
